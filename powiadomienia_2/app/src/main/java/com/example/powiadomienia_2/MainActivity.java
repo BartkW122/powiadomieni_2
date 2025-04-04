85f5f850caccb6107525e0d0d84b0210e71bd5e7
Bartosz Wiaderny <10376794u@qed.local>
2025-04-04 11:34:10 +0200

    dodanie powiadomien

--- powiadomienia_2/app/src/main/java/com/example/powiadomienia_2/MainActivity.java
+++ powiadomienia_2/app/src/main/java/com/example/powiadomienia_2/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.powiadomienia_2;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -29,8 +31,25 @@ public class MainActivity extends AppCompatActivity {
         btn2=findViewById(R.id.button2);
         btn3=findViewById(R.id.button3);
 
+
+        createNotificationChannel();
         btn1.setOnClickListener(view -> BigTextStyle() );
         btn2.setOnClickListener(view -> BigTextStyle() );
+        btn3.setOnClickListener(view ->InboxStyle());
+    }
+    private void createNotificationChannel(){
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+
+            CharSequence name = "kanal_powaidomien";
+            String opsi_kanalu = "opis_kanalu_powiadomien";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+            NotificationChannel  channel = new NotificationChannel(Channel_ID,name,importance);
+            channel.setDescription(opsi_kanalu);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
     private  void  BigTextStyle(){
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
@@ -51,8 +70,22 @@ public class MainActivity extends AppCompatActivity {
                 requestPermissions(new String[]{android.Manifest.permission.POST_NOTIFICATIONS}, 1);
                 return;
             }
-            Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.)
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(this,Channel_ID).setContentTitle("BigTextStyle").setStyle(new NotificationCompat.BigTextStyle().bigText("cewrewrwerwerwevsmvdflkvndfvndfvnjdfnbjdbnldbnlfgbnlfgbnlfgnblfbfnbdfbgdfnbdfngbjbdfgnbjldfbnjdfbn")).setPriority(NotificationCompat.PRIORITY_DEFAULT).setAutoCancel(true);
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.dice1);
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this,Channel_ID).setSmallIcon(R.drawable.dice1).setContentTitle("BigPictureStyle").setStyle(new NotificationCompat.BigPictureStyle().bigLargeIcon(bitmap)).setPriority(NotificationCompat.PRIORITY_DEFAULT).setAutoCancel(true);
+            NotificationManagerCompat notificationM= NotificationManagerCompat.from(this);
+            notificationM.notify(1,builder.build());
+
+        }
+    }
+
+    private  void InboxStyle(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{android.Manifest.permission.POST_NOTIFICATIONS}, 1);
+                return;
+            }
+
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this,Channel_ID).setSmallIcon(R.drawable.dice1).setContentTitle("InboxStyle").setStyle(new NotificationCompat.InboxStyle().addLine("linia 1").addLine("Linia 2").addLine("Linia 3")).setPriority(NotificationCompat.PRIORITY_DEFAULT).setAutoCancel(true);
             NotificationManagerCompat notificationM= NotificationManagerCompat.from(this);
             notificationM.notify(1,builder.build());
 
